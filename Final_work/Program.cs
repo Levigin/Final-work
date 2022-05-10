fd3beb74f6b78c5dadb983b5616dd5dee1d71419
Evgeniy <serganates@gmail.com>
2022-05-10 18:14:38 +0300

    Solved the task

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Task
+{
+    static void Main(string[] args)
+    {
+        string[] vs = { "hello", "2", "=)" };
+        string[] vs2 = { "124325", "-2", "2356632", "computer science" };
+
+        Arr(vs);
+        Arr(vs2);
+
+    }
+    public static string[] Arr(string[] nums)
+    {
+        int count = 0;
+        string[] array = new string[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i].Length <= 3)
+            {
+                array[count++] = nums[i];
+            }
+        }
+        Console.WriteLine(String.Join(' ', array));
+        return array;
+    }
+}
